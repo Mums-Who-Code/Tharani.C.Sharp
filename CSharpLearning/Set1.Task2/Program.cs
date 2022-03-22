0550a8b12479fc42ee85b877509e5447c3db8c06
Tharani <tharanisantosh@outlook.com>
2022-03-22 05:56:28 +0200

    C# Fundamentals : Set1 - Task2

---
+++ CSharpLearning/Set1.Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Set1.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hi, I am c#.\n");
+            Console.WriteLine("My age is 23. \nI score 99.8% in the programming.");
+            Console.ReadLine();
+        }
+    }
+}
