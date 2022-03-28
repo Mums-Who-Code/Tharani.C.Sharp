808ff258338c1b51fb4ae6d4dedcda05e836b828
Tharani <tharanisantosh@outlook.com>
2022-03-28 08:36:51 +0200

    CODE RUB : Created Projects

---
+++ CSharpLearning/Set1.Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Set1.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
