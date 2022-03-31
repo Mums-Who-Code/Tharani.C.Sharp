da3a895bca08a9211b2f8d3be5c202e04b2c64bc
Tharani <tharanisantosh@outlook.com>
2022-03-31 06:28:16 +0200

    C# Fundamentals : Set1 - Task4

--- CSharpLearning/Set1.Task4/Program.cs
+++ CSharpLearning/Set1.Task4/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 
-namespace Set1.Task4
+namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 9;
+            int b = 5;
+            int c;
+
+            Console.WriteLine("a value : " + a);
+            Console.WriteLine("b value : " + b);
+
+            c = a + b;
+            Console.WriteLine("Add a and b values : " + c);
+
+            a = ++a;
+            b = --b;
+            Console.WriteLine("\na value : " + a);
+            Console.WriteLine("b value : " + b);
+
+            c = a - b;
+            Console.WriteLine("Subtract a and b values : " + c);
+
+            a = ++a;
+            b = --b;
+            Console.WriteLine("\na value : " + a);
+            Console.WriteLine("b value : " + b);
+
+            c = a * b;
+            Console.WriteLine("Muliply a and b values : " + c);
+
+            a = ++a;
+            b = --b;
+            Console.WriteLine("\na value : " + a);
+            Console.WriteLine("b value : " + b);
+
+            c = a / b;
+            Console.WriteLine("Divide a and b values : " + c);
+            Console.ReadLine();
+
         }
     }
-}
+}        
